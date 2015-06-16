a87551748cf77df24ece69b99780d892cf5a266c
HTWG-AIN <aiabou@htwg-konstanz.de>
2015-06-16 21:33:36 +0200

    updated

--- Programmiertechnik1/Übungen/einstieg/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Paket der ersten &Uuml;bungsstunde Programmiertechnik 1 f&uuml;r AIN/1.
- * @see <a href="http://www-home.htwg-konstanz.de/~drachen/prog/Uebungen.pdf">
- *      Anleitungen zu den &Uuml;bungen</a>
- */
-
-package einstieg;
-
